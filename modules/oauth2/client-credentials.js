7981a0fa4387a0403f10de18d60745aca669133c
Valentin Laurin <me@vlaurin.eu>
2020-02-10 10:43:18 +0000

    Add OAuth2: clientAccessTokenProvider(config)()
    - Transform OAuth2 module into directory with index.js

---
+++ modules/oauth2/client-credentials.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+const DEFAULT_CACHE_TTL = 60;
+
+const newCache = () => ({});
+
+const cachedTokenProvider = (cache) => (config) => async () => getTokenFromCache(cache) || cacheToken(cache)(getTtl(config))(await getNewToken(config));
+
+/**
+ * @typedef {object} ClientAccessTokenConfig
+ * @property {number} cacheTtl Time-to-live for cache in seconds, defaults to 60
+ * @property {string} tokenEndpoint URL of the OAuth2 `/token` endpoint for the IDAM server
+ * @property {string} clientId OAuth2 client ID
+ * @property {string} clientSecret OAuth2 client secret
+ */
+/**
+ * Provides a valid OAuth2 client access token as per OAuth2's client credentials flow.
+ *
+ * @param {ClientAccessTokenConfig} config Configuration for generation of client access tokens
+ * @return {Promise} Promise resolved with the access token value
+ */
+export const clientAccessTokenProvider = (config) => cachedTokenProvider(newCache())(config);
+
+const getTokenFromCache = (cache) => cache.accessToken;
+
+const getTtl = ({cacheTtl}) => typeof cacheTtl === 'number' ? cacheTtl : DEFAULT_CACHE_TTL;
+
+const cacheToken = (cache) => (ttl) => (token) => {
+  scheduleExpiry(cache)(ttl);
+  return cache.accessToken = token;
+};
+
+const scheduleExpiry = (cache) => (ttl) => {
+  clearTimeout(cache.expire);
+  cache.expire = setTimeout(expireCache(cache), ttl  * 1000);
+};
+
+const expireCache = (cache) => () => cache.accessToken = undefined;
+
+const getNewToken = async (config) => extractToken(await requestToken(config));
+
+const extractToken = (response) => response.data.access_token;
+
+const urlSearchParams = (config) => new URLSearchParams({
+  grant_type: 'client_credentials',
+  client_id: config.clientId,
+});
+
+const authorizationHeader = (config) => 'Basic ' + Buffer.from(`${config.clientId}:${config.clientSecret}`).toString('base64');
+
+const requestToken = (config) => axios.post(config.tokenEndpoint, urlSearchParams(config), {
+  headers: {
+     authorization: authorizationHeader(config),
+  },
+});
---
+++ modules/oauth2/client-credentials.test.js
@@ -0,0 +1,86 @@
+import axios from 'axios';
+import {clientAccessTokenProvider} from './client-credentials';
+
+jest.mock('axios');
+
+describe('clientAccessTokenProvider', () => {
+
+  const config = (cacheTtl) => ({
+    cacheTtl,
+    tokenEndpoint: 'https://idam/oauth2/token',
+    clientId: 'client-123',
+    clientSecret: 'secret-123',
+  });
+
+  beforeEach(() => {
+    axios.post.mockReset();
+  });
+
+  test('should generate client access token', async () => {
+      axios.post.mockResolvedValue({data: {access_token: 'token-123'}});
+
+      const accessToken = await clientAccessTokenProvider(config(0))();
+
+      expect(accessToken).toEqual('token-123');
+
+      const lastPost = axios.post.mock.calls[axios.post.mock.calls.length - 1];
+
+      expect(lastPost[0]).toEqual('https://idam/oauth2/token');
+      expect(lastPost[1].get('grant_type')).toEqual('client_credentials');
+      expect(lastPost[1].get('client_id')).toEqual('client-123');
+      expect(lastPost[2]).toEqual({
+        headers: {
+          authorization: 'Basic Y2xpZW50LTEyMzpzZWNyZXQtMTIz',
+        }
+      });
+  });
+
+  test('should cache access token', async () => {
+    // 500ms cache
+    const provider = clientAccessTokenProvider(config(1));
+
+    axios.post.mockResolvedValue({data: {access_token: 'token-123'}});
+
+    await provider();
+
+    await new Promise((resolve) => setTimeout(resolve));
+
+    axios.post.mockResolvedValue({data: {access_token: 'token-456'}});
+
+    const accessToken = await provider();
+
+    expect(accessToken).toEqual('token-123');
+  });
+
+  test('should re-generate access token when cache expired', async () => {
+    const provider = clientAccessTokenProvider(config(0));
+
+    axios.post.mockResolvedValue({data: {access_token: 'token-123'}});
+
+    await provider();
+
+    axios.post.mockResolvedValue({data: {access_token: 'token-456'}});
+
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    const accessToken = await provider();
+
+    expect(accessToken).toEqual('token-456');
+  });
+
+  test('should cache by default when no ttl provided', async () => {
+    const provider = clientAccessTokenProvider(config());
+
+    axios.post.mockResolvedValue({data: {access_token: 'token-123'}});
+
+    await provider();
+
+    await new Promise((resolve) => setTimeout(resolve));
+
+    axios.post.mockResolvedValue({data: {access_token: 'token-456'}});
+
+    const accessToken = await provider();
+
+    expect(accessToken).toEqual('token-123');
+  });
+});

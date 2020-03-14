c2b98e69ea7a7cbf9512ed61d2ab77521cb2b363
Valentin Laurin <me@vlaurin.eu>
2020-03-14 13:34:52 +0000

    OAuth2: Add oauth2Guard(config)(req, res, next)
    - Fixes #35
    - Export AccessTokenSupplier implementation

---
+++ modules/oauth2/guard-middleware.test.js
@@ -0,0 +1,115 @@
+import {oauth2Guard} from './guard-middleware';
+
+const USER_INFO = {app_roles: 'role1,role2'};
+
+const newReq = (headers) => ({
+  get: (key) => headers[key.toLowerCase()],
+});
+
+describe('oauth2Guard', () => {
+  test('should reject with 401 when no authorization header', (done) => {
+    const req = newReq({});
+    const res = {
+      status: (code) => {
+        expect(code).toBe(401);
+        return {send: () => done()};
+      }
+    };
+    oauth2Guard()(req, res);
+  });
+
+  test('should reject with 401 when empty authorization header', (done) => {
+    const req = newReq({authorization: ''});
+    const res = {
+      status: (code) => {
+        expect(code).toBe(401);
+        return {send: () => done()};
+      }
+    };
+    oauth2Guard()(req, res);
+  });
+
+  test('should reject with 401 when malformed authorization header', (done) => {
+    const req = newReq({authorization: 'Basic xxx'});
+    const res = {
+      status: (code) => {
+        expect(code).toBe(401);
+        return {send: () => done()};
+      }
+    };
+    oauth2Guard()(req, res);
+  });
+
+  test('should reject with 401 when access token fails verification', (done) => {
+    const req = newReq({authorization: 'Bearer invalidJwtToken'});
+    const res = {
+      status: (code) => {
+        expect(code).toBe(401);
+        return {send: () => done()};
+      }
+    };
+    oauth2Guard({
+      jwtVerifier: () => Promise.reject(),
+    })(req, res);
+  });
+
+  test('should call next middleware when valid token', (done) => {
+    const req = newReq({authorization: 'Bearer validJwtToken'});
+    const res = {};
+    oauth2Guard({
+      jwtVerifier: () => Promise.resolve({}),
+    })(req, res, done);
+  });
+
+  test('should retrieve user claims when scope claim contains `profile`', (done) => {
+    const req = newReq({authorization: 'Bearer validJwtToken'});
+    const res = {};
+    const next = () => {
+      expect(req.userClaims).toEqual(USER_INFO);
+      done();
+    };
+    oauth2Guard({
+      jwtVerifier: () => Promise.resolve({'scope': 'openid profile email'}),
+      userInfoRetriever: () => Promise.resolve(USER_INFO),
+    })(req, res, next);
+  });
+
+  test('should reject with 401 when user info cannot be retrieved', (done) => {
+    const req = newReq({authorization: 'Bearer validJwtToken'});
+    const res = {
+      status: (code) => {
+        expect(code).toBe(401);
+        return {send: () => done()};
+      }
+    };
+    oauth2Guard({
+      jwtVerifier: () => Promise.resolve({'scope': 'openid profile email'}),
+      userInfoRetriever: () => Promise.reject(),
+    })(req, res);
+  });
+
+  test('should populate granted authorities for user with profile', (done) => {
+    const req = newReq({authorization: 'Bearer validJwtToken'});
+    const res = {};
+    const next = () => {
+      expect(req.grantedAuthorities).toEqual(['role1', 'role2']);
+      done();
+    };
+    oauth2Guard({
+      jwtVerifier: () => Promise.resolve({'scope': 'openid profile email'}),
+      userInfoRetriever: () => Promise.resolve(USER_INFO),
+    })(req, res, next);
+  });
+
+  test('should populate granted authorities for user without profile', (done) => {
+    const req = newReq({authorization: 'Bearer validJwtToken'});
+    const res = {};
+    const next = () => {
+      expect(req.grantedAuthorities).toEqual(['data-store/scope1', 'data-store/scope2']);
+      done();
+    };
+    oauth2Guard({
+      jwtVerifier: () => Promise.resolve({'scope': 'data-store/scope1 data-store/scope2'}),
+    })(req, res, next);
+  });
+});
--- modules/oauth2/oauth2.js
+++ modules/oauth2/oauth2.js
@@ -23,3 +23,38 @@ export const postToken = (config, params) => axios.post(
   });
 
 const authorizationHeader = (config) => 'Basic ' + Buffer.from(`${config.clientId}:${config.clientSecret}`).toString('base64');
+
+/**
+ * @callback AccessTokenSupplier
+ * @param {ExpressJsRequest} req ExpressJS request instance
+ * @return {string} Access token if found, `null` or `undefined` otherwise
+ */
+
+ /**
+  * Tries to extract an access token from a parsed cookie on an ExpressJS request.
+  *
+  * @param {string} header Name of the cookie from which access token should be extracted, defaults to `access_token`
+  * @return {AccessTokenSupplier}
+  */
+ export const cookieAccessTokenSupplier = (cookie = 'access_token') => (req) => req.cookies[cookie];
+
+/**
+ * Tries to extract an access token from a header on an ExpressJS request.
+ * The header value must comply to pattern `Bearer <accessToken>`.
+ *
+ * @param {string} header Name of the header from which access token should be extracted, defaults to `Authorization`
+ * @return {AccessTokenSupplier}
+ */
+export const headerAccessTokenSupplier = (header = 'Authorization') => (req) => {
+  const authorization = req.get(header);
+
+  if (!authorization)
+    return undefined;
+
+  const match = /^Bearer (.+)$/.exec(authorization);
+
+  if (!match)
+    return undefined;
+
+  return match[1];
+};

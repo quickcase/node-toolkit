d0f35cfdef413e1fada90982c05ac7a39abc5d77
Valentin Laurin <me@vlaurin.eu>
2020-03-15 11:30:53 +0000

    Add Document module

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 export * from './modules/case';
 export * from './modules/case-access';
+export * from './modules/document';
 export * from './modules/field';
 export * from './modules/http-client';
 export * from './modules/oauth2';

9ea04612098e29cb8482cc61670b662ac090d6ec
Valentin Laurin <me@vlaurin.eu>
2020-04-26 17:45:42 +0100

    Add module `express`

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ export * from './modules/case';
 export * from './modules/case-access';
 export * from './modules/definition';
 export * from './modules/document';
+export * from './modules/express';
 export * from './modules/field';
 export * from './modules/http-client';
 export * from './modules/oauth2';

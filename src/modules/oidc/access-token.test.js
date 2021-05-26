0ea3f4fc8562e5a0123aed60e018baea4b7936de
Valentin Laurin <valentin.laurin@quickcase.app>
2021-05-26 15:55:46 +0100

    Add simpleJwtAccessTokenParser
    Parse fixed claims from JWT tokens into consistent object

--- src/modules/oidc/access-token.test.js
+++ src/modules/oidc/access-token.test.js
@@ -1,4 +1,31 @@
-import {jwtAccessTokenVerifier} from './access-token';
+import {simpleJwtAccessTokenParser, jwtAccessTokenVerifier} from './access-token';
+
+describe('simpleJwtAccessTokenParser', () => {
+  test('should return empty scopes array when no `scope` claim', async () => {
+    const {scopes} = simpleJwtAccessTokenParser()({});
+
+    expect(scopes).toEqual([]);
+  });
+
+  test('should extract and split scopes from `scope` claim', async () => {
+    const {scopes} = simpleJwtAccessTokenParser()({scope: 'scope1 scope2'});
+
+    expect(scopes).toEqual(['scope1', 'scope2']);
+  });
+
+  test('should default clientId to undefined', async () => {
+    const {clientId} = simpleJwtAccessTokenParser()({});
+
+    expect(clientId).toBeUndefined();
+  });
+
+  test('should extract clientId from `sub` claim', async () => {
+    const {clientId} = simpleJwtAccessTokenParser()({sub: 'client-123'});
+
+    expect(clientId).toEqual('client-123');
+  });
+});
+
 
 describe('jwtAccessTokenVerifier', () => {
   test('should throw error if token cannot be verified', async () => {

56e8ba5552016dd40a5640315bfb47d935ab77d5
Valentin Laurin <valentin.laurin@quickcase.app>
2021-05-26 15:55:46 +0100

    Add simpleJwtAccessTokenParser
    Parse fixed claims from JWT tokens into consistent object

--- src/modules/oidc/access-token.js
+++ src/modules/oidc/access-token.js
@@ -1,3 +1,14 @@
+export const simpleJwtAccessTokenParser = () => (claims) => {
+
+  const mapClaim = (claim) => (map = (v) => v, defaultValue) =>
+    Object.keys(claims).includes(claim) ? map(claims[claim]) : defaultValue;
+
+  return ({
+    clientId: mapClaim('sub')(),
+    scopes: mapClaim('scope')((claim) => claim.split(' '), []),
+  });
+};
+
 export const jwtAccessTokenVerifier = ({jwtVerifier, jwtAccessTokenParser}) => async (jwtAccessToken) => {
   const claims = await jwtVerifier(jwtAccessToken);
   return jwtAccessTokenParser(claims);

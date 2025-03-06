137463d61ca31809c596c8535177b1b26a9c3860
gbendikar <giri.benadikar@quickcase.app>
2025-03-06 09:29:02 +0000

    Add support to pass request parameters to http client requests

--- src/modules/http-client.test.js
+++ src/modules/http-client.test.js
@@ -17,6 +17,21 @@ describe('httpClient', () => {
     expect(axios.get).toHaveBeenCalledWith('http://data-store:4452/path/to/resource', {headers: {Authorization: 'Bearer access-token-123'}});
   });
 
+  test('should get resource with request parameters', async () => {
+    const expectedResp = {data: {foo: 'bar'}};
+    axios.get.mockResolvedValue(expectedResp);
+
+    const resp = await httpClient(baseUrl)(tokenProviderStub).get('/path/to/resource', {['with-links']: 'true'});
+
+    expect(resp).toEqual(expectedResp);
+    expect(axios.get).toHaveBeenCalledWith('http://data-store:4452/path/to/resource',
+      {
+        headers: {Authorization: 'Bearer access-token-123'},
+        params: {['with-links']: 'true'},
+      }
+    );
+  });
+
   test('should securely post resource', async () => {
     const body = {foo: 'bar'};
     const expectedResp = {status: 201};
@@ -25,7 +40,24 @@ describe('httpClient', () => {
     const resp = await httpClient(baseUrl)(tokenProviderStub).post('/path/to/resource', body);
 
     expect(resp).toEqual(expectedResp);
-    expect(axios.post).toHaveBeenCalledWith('http://data-store:4452/path/to/resource', body,{headers: {Authorization: 'Bearer access-token-123'}});
+    expect(axios.post).toHaveBeenCalledWith('http://data-store:4452/path/to/resource', body, {headers: {Authorization: 'Bearer access-token-123'}});
+  });
+
+  test('should post resource with request parameters', async () => {
+    const body = {foo: 'bar'};
+    const expectedResp = {status: 201};
+    axios.post.mockResolvedValue(expectedResp);
+
+    const resp = await httpClient(baseUrl)(tokenProviderStub).post('/path/to/resource', body, {['computed-fields']: ['computedField1']});
+
+    expect(resp).toEqual(expectedResp);
+    expect(axios.post).toHaveBeenCalledWith('http://data-store:4452/path/to/resource',
+      body,
+      {
+        headers: {Authorization: 'Bearer access-token-123'},
+        params: {['computed-fields']: ['computedField1']},
+      }
+    );
   });
 
   test('should securely put resource', async () => {
@@ -36,7 +68,24 @@ describe('httpClient', () => {
     const resp = await httpClient(baseUrl)(tokenProviderStub).put('/path/to/resource', body);
 
     expect(resp).toEqual(expectedResp);
-    expect(axios.put).toHaveBeenCalledWith('http://data-store:4452/path/to/resource', body,{headers: {Authorization: 'Bearer access-token-123'}});
+    expect(axios.put).toHaveBeenCalledWith('http://data-store:4452/path/to/resource', body, {headers: {Authorization: 'Bearer access-token-123'}});
+  });
+
+  test('should put resource with request parameters', async () => {
+    const body = {foo: 'bar'};
+    const expectedResp = {status: 200};
+    axios.put.mockResolvedValue(expectedResp);
+
+    const resp = await httpClient(baseUrl)(tokenProviderStub).put('/path/to/resource', body, {filter: 'test'});
+
+    expect(resp).toEqual(expectedResp);
+    expect(axios.put).toHaveBeenCalledWith('http://data-store:4452/path/to/resource',
+      body,
+      {
+        headers: {Authorization: 'Bearer access-token-123'},
+        params: {filter: 'test'},
+      }
+    );
   });
 
   test('should accept custom axios instance', async () => {
